8e6dab1ed283fdc3ad2e09c466b8c6a365c75927
sergiu_derecichei <d93sergiu@gmail.com>
2023-11-02 00:56:23 +0200

    Add functions solved on edabit

--- script.js
+++ script.js
@@ -3,3 +3,26 @@
 function hello() {
   return "hello edabit.com";
 }
+
+//#2
+function calcAge(age) {
+  return age * 365;
+}
+
+//3
+function howManySeconds(hours) {
+  return hours * 60 * 60;
+}
+//4
+function getArrayFirstElem(arr) {
+  return arr[0];
+}
+
+//5 
+function remainder(x, y) {
+	return x % y;
+}
+//6
+function convert(minutes) {
+	return minutes * 60;
+}
