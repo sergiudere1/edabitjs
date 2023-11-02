073181294863405508880b6ed90c696b0f9172cd
sergiu_derecichei <d93sergiu@gmail.com>
2023-11-02 10:34:28 +0200

    Very Easy Challenges - 10 pieces

---
+++ very-easy-script.js
@@ -0,0 +1,42 @@
+//Very Easy Challenges from Edabit
+
+//1
+function hello() {
+  return "hello edabit.com";
+}
+//2
+function calcAge(age) {
+  return age * 365;
+}
+//3
+function howManySeconds(hours) {
+  return hours * 60 * 60;
+}
+//4
+function getArrayFirstElem(arr) {
+  return arr[0];
+}
+//5
+function remainder(x, y) {
+  return x % y;
+}
+//6
+function convert(minutes) {
+  return minutes * 60;
+}
+//7
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+//8
+function nextEdge(side1, side2) {
+  return side1 + side2 - 1;
+}
+//9
+function addition(num) {
+  return num + 1;
+}
+//10
+function addition(a, b) {
+  return a + b;
+}
