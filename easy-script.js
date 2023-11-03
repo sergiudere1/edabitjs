d329c34075c92bc4ff4c62c27a4343de37e81ba1
sergiu_derecichei <d93sergiu@gmail.com>
2023-11-03 18:30:45 +0200

    2 easy challenges

--- easy-script.js
+++ easy-script.js
@@ -4,6 +4,7 @@ function addUp(num) {
   for (i = 1; i <= num; i++) nr += i;
   return nr;
 }
+
 //2
 class Person {
 	constructor(name, age) {
@@ -30,3 +31,18 @@ p3 = new Person("Lily", 24)
 p1.compareAge(p2);
 p2.compareAge(p1);
 p1.compareAge(p3);
+
+//3 
+function giveMeSomething(a) {
+	return `something ${a}`
+}
+
+//4
+function minMax(arr) {
+	let minMaxArr = [];
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+	minMaxArr.push(min);
+	minMaxArr.push(max);
+	return minMaxArr;
+}
